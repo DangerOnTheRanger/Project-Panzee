bd961cfb2a1489e255cdaba1b76a61a93c2085bd
DangerOnTheRanger <tuxfreak@tuxfamily.org>
2016-01-08 13:51:04 -0600

    Better to use string data instead of a file reference

--- test/test_unit.py
+++ test/test_unit.py
@@ -3,7 +3,7 @@ import panzee.damage
 
 
 def test_load():
-    unit = panze.unit.Unit(unit_base=open("example_unit.udl"))
+    unit = panze.unit.Unit(unit_base=open("example_unit.udl").read())
     assert unit.max_ap == 150
     assert unit.ap == 150
     assert unit.status == panzee.damage.STATUS_NORMAL
